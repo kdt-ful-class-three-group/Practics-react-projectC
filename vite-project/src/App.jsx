2764b7ee7d0789a32aa0f516173339fecff188e0
김민지 <qniomo00@gmail.com>
2025-03-31 13:08:44 +0900

    라우터 설정을 위해 모듈 가져옴

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './App.css'
+import Counter from './Counter'
+
 
 function App() {
   let count = 0
