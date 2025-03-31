fbf6a38ac2b418b7200d862a263b203efc2a22e1
김민지 <qniomo00@gmail.com>
2025-03-31 14:10:47 +0900

    라우터 설정

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,7 +4,24 @@ import Counter from './Counter' //카운터 페이지
 
 
 function App() {
-  
+  return (
+    <Router>
+      <Routes>
+        {/* 첫페이지 */}
+        <Route path='/' element={
+          <div className='first-page'>
+            <h1>COUNTER</h1>
+            {/* 첫페이지에서 카운터 페이지로 이동 */}
+            <Link to='/counter'>
+              <button>시작하기</button>
+            </Link>
+          </div>
+        } />
+        {/* 카운터 페이지 */}
+        <Route path='/counter' element={<Counter/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
