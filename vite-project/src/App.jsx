a369a0851fa177089efe75f4fe0c3f94630c3e09
김민지 <qniomo00@gmail.com>
2025-03-31 12:38:08 +0900

    버튼에 함수 적용

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,9 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  let count = 0
+
+  function addCount() {
+    count += 1
+    document.getElementsByTagName('h1')[0].innerHTML=count
+  }
+  function minusCount() {
+    count -= 1
+    if (count < 0) {
+      count = 0
+    }
+    document.getElementsByTagName('h1')[0].innerHTML=count
+  }
+  
   return <div>
-    <h1>0</h1>
-    <button>+</button>
+    <h1>{count}</h1>
+    <button onClick={addCount}>+</button>
+    <button onClick={minusCount}>-</button>
   </div>
 }
 
