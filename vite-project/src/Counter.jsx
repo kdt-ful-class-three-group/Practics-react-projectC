346997afb148170922b452565f2f0b20872eacae
김민지 <qniomo00@gmail.com>
2025-03-31 15:51:21 +0900

    함수형 컴포넌트로 작성

--- vite-project/src/Counter.jsx
+++ vite-project/src/Counter.jsx
@@ -1,27 +1,23 @@
 import React, { useState } from 'react'
 
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {count:0} // count를 state로 관리
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  const addCount = () => {
+    setCount(prevCount => prevCount +1)
   }
 
-  addCount = () => {
-    this.setState(prevState => ({count:prevState.count+1})) //state 변경
+  const minusCount = () => {
+    setCount(prevCount => Math.max(prevCount-1 , 0))
   }
 
-  minusCount = () => {
-    this.setState(prevState => ({count:Math.max(prevState.count -1, 0)}))// 0보다 작으면 0이 할당됨
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.addCount}>+</button>
-        <button onClick={this.minusCount}>-</button>
-      </div>)
-  }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={addCount}>+</button>
+      <button onClick={minusCount}>-</button>
+    </div>
+  )
 }
 
 export default Counter
