582f49371e0c92c4d7258b62a5ad5b489c24c709
김민지 <qniomo00@gmail.com>
2025-03-31 13:08:19 +0900

    페이지 이동을 위해 Counter.jsx파일 따로 만들어줌

---
+++ vite-project/src/Counter.jsx
@@ -0,0 +1,23 @@
+function Counter() {
+  let count = 0
+
+  function addCount() {
+    count += 1
+    document.getElementsByTagName('h1')[0].innerHTML=count
+  }
+  function minusCount() {
+    count -= 1
+    if (count < 0) {
+      count = 0
+    }
+    document.getElementsByTagName('h1')[0].innerHTML=count
+  }
+  
+  return <div>
+    <h1>{count}</h1>
+    <button onClick={addCount}>+</button>
+    <button onClick={minusCount}>-</button>
+  </div>
+}
+
+export default Counter
